000583b38a545b4d50cc0c472d30666627c105f8
Ilina <ilina.tsoneva3@gmail.com>
2021-07-11 09:59:48 +0300

    Add custom auth state provider

--- IT.Valor/Client/Services/ApiAuthenticationStateProvider.cs
+++ IT.Valor/Client/Services/ApiAuthenticationStateProvider.cs
@@ -1,38 +1,118 @@
 ﻿using System;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
+using IT.Valor.Common.Helpers;
 using IT.Valor.Common.Models;
+using IT.Valor.Common.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace IT.Valor.Client.Services
 {
     public class ApiAuthenticationStateProvider : AuthenticationStateProvider, IApiAuthenticationStateProvider
     {
-        private readonly ILocalStorageService _localStorageService;
+        private bool firstTime = true;
 
-        public ApiAuthenticationStateProvider(ILocalStorageService localStorageService)
+        private readonly ILocalStorageService _localStorageService;
+        private readonly ICustomHttpClient _httpClient;
+
+        public ApiAuthenticationStateProvider(ILocalStorageService localStorageService,
+            ICustomHttpClient httpClient)
         {
             _localStorageService = localStorageService;
+            _httpClient = httpClient;
         }
 
         public async Task AuthenticateUser(LoginInput loginResult)
         {
+            var token = await ParseToken(loginResult);
+            await AuthenticateUser(token);
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
             var token = await GetTokenAsync();
-        }
 
-        private async Task<SavedToken> GetTokenAsync()
-        {
-            throw new NotImplementedException();
-        }
+            if (string.IsNullOrWhiteSpace(token.LoginResult.Token))
+            {
+                firstTime = false;
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
 
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
-        {
-            throw new NotImplementedException();
+            if (firstTime)
+            {
+                firstTime = false;
+                await AuthenticateUser(token);
+            }
+
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(token.Claims, "jwt")));
         }
 
         public async Task LogOutUser()
         {
-            throw new NotImplementedException();
+            var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+            var authState = Task.FromResult(new AuthenticationState(anonymousUser));
+
+            await _localStorageService.RemoveItemAsync("authToken");
+            await _localStorageService.RemoveItemAsync("tokenExpire");
+
+            _httpClient.SetToken(string.Empty);
+
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        private async Task<SavedToken> GetTokenAsync()
+        {
+            var token = await _localStorageService.GetItemAsync<string>("authToken");
+            var expDate = await _localStorageService.GetItemAsync<DateTime>("tokenExpire");
+            return await ParseToken(new LoginInput
+            {
+                Token = token,
+                ExpirationDate = expDate
+            });
+        }
+
+        private async Task AuthenticateUser(SavedToken token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.NameIdentifier, token.LoginResult.UserId) }, "apiauth"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+
+            await _localStorageService.SetItemAsync("authToken", token.LoginResult.Token);
+            await _localStorageService.SetItemAsync("tokenExpire", token.LoginResult.ExpirationDate);
+
+            _httpClient.SetToken(token.LoginResult.Token);
+
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        private async Task<SavedToken> ParseToken(LoginInput loginInput)
+        {
+            if (string.IsNullOrEmpty(loginInput.Token))
+            {
+                return new SavedToken();
+            }
+
+            var isExpired = loginInput.ExpirationDate < DateTime.UtcNow;
+
+            if (isExpired)
+            {
+                await LogOutUser();
+                return new SavedToken();
+            }
+
+            var claims = JwtParserHelper.ParseClaimsFromJwt(loginInput.Token);
+            var userId = claims.FirstOrDefault(x => x.Type == "nameid").Value;
+            return new SavedToken
+            {
+                Claims = claims,
+                LoginResult = new LoginInput
+                {
+                    UserId = userId,
+                    ExpirationDate = loginInput.ExpirationDate,
+                    Token = loginInput.Token
+                }
+            };
         }
     }
 }
