38d55b4160dc2a560ccb3f982b72efec1e4ecfb6
Ilina <ilina.tsoneva3@gmail.com>
2021-07-16 16:34:42 +0300

    Add BaseRepository

---
+++ IT.Valor.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace IT.Valor.Core.Interfaces.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(string id);
+
+        Task<TEntity> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<TEntity> SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        void DeleteRange(IEnumerable<TEntity> entities);
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ IT.Valor.Infrastructure/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using IT.Valor.Core.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT.Valor.Infrastructure.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly DbContext _dbContext;
+
+        public BaseRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ApplicationDbContext Context => _dbContext as ApplicationDbContext;
+
+        public async Task<TEntity> GetByIdAsync(string id)
+            => await Context.Set<TEntity>().FindAsync(id);
+
+        public async Task<TEntity> GetByIdAsync(Guid id)
+           => await Context.Set<TEntity>().FindAsync(id);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+            => await Context.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+            => await Context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+
+        public async Task<TEntity> SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+            => await Context.Set<TEntity>().SingleOrDefaultAsync(predicate);
+
+        public async Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+            => await Context.Set<TEntity>().Where(predicate).ToListAsync();
+
+        public void Add(TEntity entity)
+            => Context.Set<TEntity>().Add(entity);
+
+        public void Update(TEntity entity)
+            => Context.Set<TEntity>().Update(entity);
+
+        public void Delete(TEntity entity)
+            => Context.Set<TEntity>().Remove(entity);
+
+        public void AddRange(IEnumerable<TEntity> entities)
+            => Context.Set<TEntity>().AddRange(entities);
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+            => Context.Set<TEntity>().UpdateRange(entities);
+
+        public void DeleteRange(IEnumerable<TEntity> entities)
+            => Context.Set<TEntity>().RemoveRange(entities);
+
+        public async Task SaveChangesAsync()
+            => await Context.SaveChangesAsync();
+    }
+}
